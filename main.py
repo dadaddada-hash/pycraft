c87ec3e38be9c8533d9ffe6a7433ad7072fa005a
dadaddada-hash <dablobabablo@protonmail.com>
2024-11-03 14:03:36 +0100

    more code

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 #import libarys
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 #create an instance of the ursina app
 app = Ursina()
 
+#create player
+player = FirstPersonController(
+    mouse_sensitivity=Vec2(100, 100)
+)
+
 #create the ground
-ground = Entity()
+ground = Entity(
+    model="plane",
+    scale=(100, 1, 100),
+    Texture="grass",
+    texture_scale=(10, 10),
+    Collider="true"
+)
 
 #run the app
 app.run()
